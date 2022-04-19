1a25dc2279e82f86c9549e1a8ce1409828888197
Nciklol <itzthetester@gmail.com>
2022-04-19 17:15:22 +0000

    remove(getEnv): unnecessary utility

--- src/client/Client.ts
+++ src/client/Client.ts
@@ -1,13 +1,12 @@
 import { Gateway } from '#client/ws/Gateway';
 import type { Guild } from '#structures/Guild';
-import { getEnv } from '#utils/getEnv';
 import type { Channel } from '#structures/Channel';
 import { EventEmitter } from 'stream';
 import type { ClientOptions } from '../types/LIB';
 
 export class Client extends EventEmitter {
 	public gateway: Gateway;
-	public token: string = getEnv('DISCORD_TOKEN');
+	public token = process.env.DISCORD_TOKEN as string;
 	public guilds = new Map<string, Guild>();
 	public channels = new Map<string, Channel>();
 
