056a6e65d6b7ea123af6be319daab328c1715065
tristan <69066026+twisttaan@users.noreply.github.com>
2022-04-20 09:35:46 +1000

    chore: lint (#12)

--- src/structures/Embed.ts
+++ src/structures/Embed.ts
@@ -1,25 +1,6 @@
+// eslint-disable-next-line @typescript-eslint/no-extraneous-class
 export default class Embed {
-    embed: { title: null | String; type: string; description: null | String; url: null | String; timestamp: null; color: null; footer: null | Object; image: null | Object; thumbnail: null |Object; video: null | Object; provider: null | Object; author: null | Object; fields: []; };
-    public constructor() {
-		this.embed = {
-            title: null,
-            type: "rich",
-            description: null,
-            url: null,
-            timestamp: null,
-            color: null,
-            footer: null,
-            image: null,
-            thumbnail: null,
-            video: null,
-            provider: null,
-            author: null,
-            fields: []
-        }
+	public constructor() {
+		throw new Error('Not implemented yet https://github.com/TeamAilurus/ailurus/pull/3');
 	}
-
-    public setTitle(title: String) {
-        if(typeof title !== "string") throw new Error(`Expected type string but got ${typeof title} for embed title`)
-        this.embed.title = title
-    }
 }
