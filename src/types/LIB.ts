5141f2aa9af13426355e38e1e0684eb6f64cd6fa
Baltraz <ybaltraz@gmail.com>
2022-04-19 11:57:52 +0000

    feat: embed builder

--- src/types/LIB.ts
+++ src/types/LIB.ts
@@ -2,3 +2,19 @@ export interface ClientOptions {
 	token: string;
 	intents: number;
 }
+
+export interface EmbedOptions {
+	title: null | String
+	type: "rich"
+	description: null | String
+	url: null | String
+	timestamp: null //idk how to type this
+	color: String | null
+	footer: null | Object
+	image: null | Object
+	thumbnail: null | Object
+	video: null | Object
+	provider: null | Object
+	author: null | Object
+	fields: []
+}
