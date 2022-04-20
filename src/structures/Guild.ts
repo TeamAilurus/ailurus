a0d5581574de101180b05cd64c64aeb74088a746
Tristan Camejo <twisttaan@icloud.com>
2022-04-20 15:34:11 +1000

    refactor: readonly and var defines

--- src/structures/Guild.ts
+++ src/structures/Guild.ts
@@ -1,18 +1,15 @@
 import type { Client } from '#client/Client';
 import { Base } from '#structures/Base';
 import { Channel } from '#structures/Channel';
-import type { APIGuild } from 'discord-api-types/v10';
+import type { APIGuild, Snowflake } from 'discord-api-types/v10';
 
 export class Guild extends Base {
-	public id: string;
-	public name: string;
+	public readonly id: Snowflake = this.data.id;
+	public name: string = this.data.name;
 	public channels: Map<string, Channel>;
 
 	public constructor(public data: APIGuild, client: Client) {
 		super(client);
-
-		this.id = data.id;
-		this.name = data.name;
 		this.channels = new Map(data.channels ? data.channels.map((chan: any) => [chan.id, new Channel(chan, this, client)]) : []);
 	}
 }
