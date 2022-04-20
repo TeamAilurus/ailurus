66a7e92422ec5c554615cb1d2bed173560f07521
Tristan Camejo <twisttaan@icloud.com>
2022-04-20 12:50:21 +1000

    feat: embed test

--- src/tests/index.ts
+++ src/tests/index.ts
@@ -5,8 +5,17 @@ import { Client } from '..';
 const client = new Client();
 
 client.on('message', (message: Message) => {
-	if (message.content === 'ai!ping')
+	if (message.content === 'test message')
 		void message.reply({
 			content: 'pong!'
+		});
+
+	if (message.content === 'test embed')
+		void message.reply({
+			embeds: [
+				{
+					description: 'hey an embed'
+				}
+			]
 		});
 });
