2401a089dc5ac65c59c01152cdd98dcf7a097f8d
Kurp <78627265+Kurpp@users.noreply.github.com>
2022-04-20 17:25:34 -0500

    refactor: Apply chewies/nick's suggestion

--- src/structures/GuildMember.ts
+++ src/structures/GuildMember.ts
@@ -7,8 +7,8 @@ import { Base } from './Base';
 export class GuildMember extends Base {
 	public readonly id = this.user.id;
 
-	public nick = this.raw.nick;
-	public avatar = this.raw.avatar;
+	public avatar: string | null = this.raw.avatar ?? null;
+	public nickname: string | null = this.raw.nick ?? null;
 	public roles = new Map(this.raw.roles.length ? this.raw.roles.map((rid: string) => [rid, this.guild.roles.get(rid)]) : []);
 
 	public constructor(private raw: APIGuildMember, public user: User, public guild: Guild, client: Client) {
